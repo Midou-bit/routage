64d429247c9166c6eda59a7a813168fd7995d496
midou-bit <midou@DESKTOP-GEB5SA4>
2025-05-12 16:51:15 +0200

    activite7

--- routage/src/components/Header.jsx
+++ routage/src/components/Header.jsx
@@ -1,12 +1,17 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
-import { NavLink } from 'react-router'
-import '../assets/styles/header.css' 
+import { NavLink, useMatch } from 'react-router'
+import '../assets/styles/header.css'
 
 function Header() {
+  const match = useMatch('/category/:categoryId')
+
   return (
     <header>
       <Navbar bg="light" expand="lg" className="border-bottom">
         <Container>
+          {match && (
+            <span className="me-3">Catégorie : {match.params.categoryId}</span>
+          )}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
             <Nav>
