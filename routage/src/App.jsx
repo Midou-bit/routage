084fedbb80409bc6c4573c0c235bfbae5387981d
midou-bit <midou@DESKTOP-GEB5SA4>
2025-05-12 13:58:01 +0200

    activite2

--- routage/src/App.jsx
+++ routage/src/App.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router'
+import Layout from './components/Layout'
 import Home from './pages/Home'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   )
 }
