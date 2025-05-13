73b46239bd8cc698d94564f79bf6ab99c5c9817d
midou-bit <midou@DESKTOP-GEB5SA4>
2025-05-13 12:25:04 +0200

    activite8

--- routage/src/pages/Category.jsx
+++ routage/src/pages/Category.jsx
@@ -1,9 +1,17 @@
-import { useParams } from 'react-router'
+import { useParams, useNavigate } from 'react-router'
 
 function Category() {
   const { categoryId } = useParams()
+  const navigate = useNavigate()
 
-  return <h1>Catégorie : {categoryId}</h1>
+  return (
+    <div className="text-center mt-4">
+      <h1>Catégorie : {categoryId}</h1>
+      <button onClick={() => navigate('/')} className="btn btn-primary mt-3">
+        Retour à la page d'accueil
+      </button>
+    </div>
+  )
 }
 
 export default Category
